7d7f6e2617e6ac1058557f564600fbc57634183a
Chad Ostrowski <hi@chadoh.com>
2018-02-02 14:08:59 -0500

    Add activerecord

--- models/student.rb
+++ models/student.rb
@@ -1,28 +1,9 @@
-require "sqlite3"
+require "active_record"
 
-class Student
-  attr_accessor :id, :name, :email, :discord
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/dev.db",
+)
 
-  DB = SQLite3::Database.new "./db/dev.db"
-
-  def initialize(id, name, email, discord)
-    @id = id
-    @name = name
-    @email = email
-    @discord = discord
-  end
-
-  def self.all
-    student_array = DB.execute("SELECT * FROM students")
-    student_array.map do |id, name, email, discord|
-      Student.new(id, name, email, discord)
-    end
-  end
-
-  def self.find(id)
-    student = DB.execute(
-      "SELECT * FROM students WHERE id=?", id
-    ).first
-    Student.new(student[0], student[1], student[2], student[3])
-  end
+class Student < ActiveRecord::Base
 end
